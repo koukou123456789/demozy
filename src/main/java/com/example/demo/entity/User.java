aba8f262b889e9605d44ef2a446d7a20402d7da8
zy <1029843256@qq.com>
2021-10-23 20:52:18 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,35 @@
+package com.example.demo.entity;
+
+
+    public class User {
+        private int id;
+        private String name;
+        private String hobby;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getHobby() {
+            return hobby;
+        }
+
+        public void setHobby(String hobby) {
+            this.hobby = hobby;
+        }
+
+
+    }
+
